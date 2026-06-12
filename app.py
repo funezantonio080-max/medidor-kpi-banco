4e03662ffdb36fb216a6c6da7aa0faf0c545b53a
funezantonio080-max <funezantonio080@gmail.com>
2026-06-12 16:47:06 -0600

    Update app.py

--- app.py
+++ app.py
@@ -264,6 +264,24 @@ if menu == "DASHBOARD":
     empleados = pd.read_sql("SELECT * FROM empleados", conn)
     kpis = pd.read_sql("SELECT * FROM kpis", conn)
 
+    lista_cargos = sorted(
+    empleados["cargo"].dropna().unique().tolist()
+)
+
+cargo_filtro = st.selectbox(
+    "📋 FILTRAR POR CARGO",
+    ["TODOS"] + lista_cargos
+)
+
+if cargo_filtro != "TODOS":
+
+    ids_empleados = empleados[
+        empleados["cargo"] == cargo_filtro
+    ]["id"].tolist()
+
+    kpis = kpis[
+        kpis["id"].isin(ids_empleados)
+
     total_emp = len(empleados)
     total_kp = len(kpis)
     prom_cump = round((kpis["real"].sum() / (kpis["meta"].sum() + 1)) * 100, 2) if not kpis.empty else 0
